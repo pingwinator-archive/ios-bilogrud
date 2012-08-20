471ff0942f28538267d8840d756d0802bf5d9bcd
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2012-08-20 15:40:16 +0300

    hello world with custom icon

---
+++ Hello World/Hello World/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Natasha on 20.08.12.
+//  Copyright (c) 2012 Natasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
