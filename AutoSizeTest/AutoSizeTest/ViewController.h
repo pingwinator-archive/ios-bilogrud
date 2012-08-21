6ee32b4bf7f37031a278e9d11ac06b7b68dc62a5
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2012-08-21 15:18:10 +0300

    handle resize buttons

--- AutoSizeTest/AutoSizeTest/ViewController.h
+++ AutoSizeTest/AutoSizeTest/ViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *buttonLR;
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *buttonLL;
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *buttonR;
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *buttonL;
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *buttonUR;
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *buttonUL;
 
 @end
--- AutoSizeTest/AutoSizeTest/ViewController.m
+++ AutoSizeTest/AutoSizeTest/ViewController.m
@@ -13,6 +13,12 @@
 @end
 
 @implementation ViewController
+@synthesize buttonLR;
+@synthesize buttonLL;
+@synthesize buttonR;
+@synthesize buttonL;
+@synthesize buttonUR;
+@synthesize buttonUL;
 
 - (void)viewDidLoad
 {
@@ -22,13 +28,64 @@
 
 - (void)viewDidUnload
 {
+    [self setButtonUL:nil];
+    [self setButtonUR:nil];
+    [self setButtonL:nil];
+    [self setButtonR:nil];
+    [self setButtonLL:nil];
+    [self setButtonLR:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    return YES;// (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+-(void) willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration{
+  
+    if (UIInterfaceOrientationPortraitUpsideDown == toInterfaceOrientation) {
+        NSLog( @"UIInterfaceOrientationPortrait");
+
+
+    }
+    if (UIInterfaceOrientationLandscapeLeft == toInterfaceOrientation) {
+        NSLog( @"UIInterfaceOrientationLandscapeLeft");
+    }
+    if (UIInterfaceOrientationLandscapeRight == toInterfaceOrientation) {
+        NSLog( @"UIInterfaceOrientationLandscapeRight");
+    }
+    
+    if(UIInterfaceOrientationIsPortrait(toInterfaceOrientation)){
+        buttonUL.frame = CGRectMake(20, 20, 125, 125);
+        buttonUR.frame = CGRectMake(170, 20, 125, 125);
+//        buttonL.frame = CGRectMake(20, 165, 125, 125);
+//        buttonR.frame = CGRectMake(170, 165, 125, 125);
+        NSLog( @"to portrait! %d", 5 );
+        buttonLL.frame = CGRectMake(20, 308, 125, 125);
+        buttonLR.frame = CGRectMake(170, 308, 125, 125);
+        
+    }
+    else{
+        if(UIInterfaceOrientationIsLandscape(toInterfaceOrientation)){
+        NSLog( @"smth! %d", 5 );
+        buttonUL.frame = CGRectMake(20, 20, 125, 125);
+        buttonUR.frame = CGRectMake(20, 165, 125, 125);
+//        buttonL.frame = CGRectMake(20, 165, 125, 125);
+//        buttonR.frame = CGRectMake(165, 165, 125, 125);
+        buttonLL.frame = CGRectMake(308, 20, 125, 125);
+        buttonLR.frame = CGRectMake(308, 165, 125, 125);
+    }
+    }
 }
 
+- (void)dealloc {
+    [buttonUL release];
+    [buttonUR release];
+    [buttonL release];
+    [buttonR release];
+    [buttonLL release];
+    [buttonLR release];
+    [super dealloc];
+}
 @end
