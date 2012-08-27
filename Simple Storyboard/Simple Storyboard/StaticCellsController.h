d490df6330cf6a2ff58214453e0ad2324adc8291
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2012-08-27 11:39:00 +0300

    StoryBoard example. NSRange. static Cell

---
+++ Simple Storyboard/Simple Storyboard/StaticCellsController.h
@@ -0,0 +1,15 @@
+//
+//  StaticCellsController.h
+//  Simple Storyboard
+//
+//  Created by Natasha on 27.08.12.
+//  Copyright (c) 2012 Natasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StaticCellsController : UITableViewController
+@property (retain, nonatomic) IBOutlet UILabel *timeLabel;
+@property (retain, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
