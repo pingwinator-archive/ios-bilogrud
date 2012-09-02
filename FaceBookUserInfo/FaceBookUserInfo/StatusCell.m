4b40800d3bd6e84161e6cdb457f9b96661984b5f
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2012-09-03 00:23:31 +0300

    add list of statuses

---
+++ FaceBookUserInfo/FaceBookUserInfo/StatusCell.m
@@ -0,0 +1,29 @@
+//
+//  StatusCell.m
+//  FaceBookUserInfo
+//
+//  Created by Natasha on 03.09.12.
+//  Copyright (c) 2012 Natasha. All rights reserved.
+//
+
+#import "StatusCell.h"
+
+@implementation StatusCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
