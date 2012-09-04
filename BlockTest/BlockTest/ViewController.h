c0b392cbd9ec06b3c6fd73a6809736d5c5a72ee0
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2012-09-04 09:13:25 +0300

    add dispatch async.

---
+++ BlockTest/BlockTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlockTest
+//
+//  Created by Natasha on 03.09.12.
+//  Copyright (c) 2012 Natasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
