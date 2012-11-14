1565790f31cf7c40267ca15429a7b727841b1098
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2012-11-14 12:24:16 +0200

    add files GameViewController

---
+++ TetrisNew/TetrisNew/ViewControllers/GameCenterViewController.m
@@ -0,0 +1,38 @@
+//
+//  GameCenterViewController.m
+//  TetrisNew
+//
+//  Created by Natasha on 14.11.12.
+//  Copyright (c) 2012 Natasha. All rights reserved.
+//
+
+#import "GameCenterViewController.h"
+
+@interface GameCenterViewController ()
+
+@end
+
+@implementation GameCenterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
