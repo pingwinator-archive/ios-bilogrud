ecee5590419d57d43ab59106e3408d1305c21acc
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2013-01-24 14:41:06 +0200

    update animation

--- AnimationView/AnimationView/ViewController.m
+++ AnimationView/AnimationView/ViewController.m
@@ -14,6 +14,7 @@
 @property (strong, nonatomic) UIButton* animationButton;
 @property (strong, nonatomic) UIScrollView* horisontalScrollView;
 @property (strong, nonatomic) UIView* bgView;
+@property (strong, nonatomic) NSMutableArray* viewsCollection;
 @end
 
 @implementation ViewController
@@ -31,24 +32,14 @@
     self.bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 180, 320, 100)];
     self.bgView.backgroundColor = [UIColor yellowColor];
     [self.view addSubview:self.bgView];
-//    [self.view addSubview:[self addHorisontalScrollView]];
     
-//    for (NSInteger i = 0; i < 10; i++) {
-//        
-//        UIView* testView = [[UIView alloc] initWithFrame:CGRectMake(i*menuItemWidth+i*5, 200, menuItemWidth , menuItemWidth)];
-//        testView.backgroundColor = [UIColor redColor];
-//        [self.view addSubview:testView];
-//        
-//        [UIView animateWithDuration:1 animations:^{
-//            testView.center = CGPointMake(100, 300);
-//        } completion:^ (BOOL finished) {
-//             if (finished) {
-//                 // Revert image view to original.
-//                 NSLog(@"Is completed");
-//                 testView.center = CGPointMake(i*menuItemWidth+i*5, 200);
-//        }
-//         }];
-//    }
+    self.viewsCollection = [NSMutableArray array];
+    
+    for (NSInteger i = 0; i < 10; i++) {
+        UIView* testView = [[UIView alloc] initWithFrame:CGRectMake(i*menuItemWidth+i*15, 20, menuItemWidth , menuItemWidth)];
+        testView.backgroundColor = [UIColor redColor];
+        [self.viewsCollection addObject:testView];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -68,27 +59,60 @@
 
 - (void)showAnimation
 {
-    [self.bgView removeFromSuperview];
-    self.bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 200, 320, 100)];
-    self.bgView.backgroundColor = [UIColor yellowColor];
-    [self.view addSubview:self.bgView];
-    for (NSInteger i = 0; i < 10; i++) {
-      
-        UIView* testView = [[UIView alloc] initWithFrame:CGRectMake(i*menuItemWidth+i*15, 20, menuItemWidth , menuItemWidth)];
-        testView.backgroundColor = [UIColor redColor];
-        
+//    [self.bgView removeFromSuperview];
+//    self.bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 200, 320, 100)];
+//    self.bgView.backgroundColor = [UIColor yellowColor];
+//    [self.view addSubview:self.bgView];
+//    for (NSInteger i = 0; i < 10; i++) {
+//      
+//        UIView* testView = [[UIView alloc] initWithFrame:CGRectMake(i*menuItemWidth+i*15, 20, menuItemWidth , menuItemWidth)];
+//        testView.backgroundColor = [UIColor redColor];
+//        
+//        [UIView animateWithDuration:0.2 animations:^{
+//            testView.frame = CGRectMake(testView.frame.origin.x-5, testView.frame.origin.y-5, menuItemWidth+10, menuItemWidth+10);
+//        } completion:^ (BOOL finished) {
+//            [UIView animateWithDuration:0.2 animations:^{
+//                testView.frame = CGRectMake(i*menuItemWidth+i*5, 20, menuItemWidth , menuItemWidth);
+//                             }];
+//        }];
+//        
+//        [self.bgView addSubview:testView];
+// }
+    
+    
+
+//    UIView* __block testView;
+    NSInteger __block i = 0;
+    while (i < 10) {
+        NSLog(@"i %d", i);
         [UIView animateWithDuration:0.2 animations:^{
-            testView.frame = CGRectMake(testView.frame.origin.x-5, testView.frame.origin.y-5, menuItemWidth+10, menuItemWidth+10);
+            //            testView = [self nextView:i];
+            //             [self.view addSubview:[self nextView:i]];
+            CGRect frameTemp = [self nextView:i].frame;
+            [self nextView:i].frame = CGRectMake(frameTemp.origin.x - 5, frameTemp.origin.y - 5, menuItemWidth + 10, menuItemWidth + 10);
         } completion:^ (BOOL finished) {
             [UIView animateWithDuration:0.2 animations:^{
-                testView.frame = CGRectMake(i*menuItemWidth+i*5, 20, menuItemWidth , menuItemWidth);
-                             }];
+              
+//              CGRect frameTemp = [self nextView:i].frame;
+                
+                [self nextView:i].frame = CGRectMake(i*menuItemWidth+i*5, 20, menuItemWidth , menuItemWidth);
+            }];
+//            NSLog(@"%d", i);
+//            [self.view addSubview:[self nextView:i]];
         }];
-        
-        [self.bgView addSubview:testView];
- }
+        NSLog(@"%d", i);
+        [self.view addSubview:[self nextView:i]];
 
+        i++;
+    }
+    
 }
 
+
+
+- (UIView*)nextView:(NSInteger)i
+{
+    return  (i < [self.viewsCollection count]) ? [self.viewsCollection objectAtIndex:i] : nil;
+}
 
 @end
