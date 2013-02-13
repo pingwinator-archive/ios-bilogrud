cc6250546c6ed80f8c9993c9b4fb20b016573b5b
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2013-02-13 18:04:31 +0200

    add custom activity indicator proj

---
+++ CustomActivityApp/CustomActivityApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomActivityApp
+//
+//  Created by Natasha on 13.02.13.
+//  Copyright (c) 2013 Natasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CustomActivityApp/CustomActivityApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CustomActivityApp
+//
+//  Created by Natasha on 13.02.13.
+//  Copyright (c) 2013 Natasha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
