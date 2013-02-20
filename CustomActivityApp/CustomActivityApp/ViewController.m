5fbccb40d28bee7bedcfc246c9496a366d90eba1
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2013-02-20 08:17:55 +0200

    add activity animation

--- CustomActivityApp/CustomActivityApp/ViewController.m
+++ CustomActivityApp/CustomActivityApp/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#define activitySide 50
 @interface ViewController ()
 
 @end
@@ -17,6 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self startImageViewAnimation];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -25,5 +26,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)startImageViewAnimation
+{
+    NSMutableArray *animationImages = [NSMutableArray array];
+    for (NSUInteger i = 0; i < 12; i++) {
+        [animationImages addObject:[UIImage imageNamed:[NSString stringWithFormat:@"shape_%d.png", i + 1]]];
+    }
+   
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2 - activitySide/2, self.view.frame.size.height/2 - activitySide/2, activitySide, activitySide)];
+    [self.imageView setBackgroundColor:[UIColor whiteColor]];
+    self.imageView.animationImages = animationImages;
+    self.imageView.animationRepeatCount = 0;
+    self.imageView.animationDuration= 1.2;
+   
+    [self.view addSubview:self.imageView];
+    [self.imageView startAnimating];
+} 
+
 
 @end
