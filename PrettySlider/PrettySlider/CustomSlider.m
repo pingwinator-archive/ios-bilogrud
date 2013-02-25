daf0b1d411c3d2d475d63e6ec67f895c61632d38
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2013-02-25 19:09:22 +0200

    refactor slider

--- PrettySlider/PrettySlider/CustomSlider.m
+++ PrettySlider/PrettySlider/CustomSlider.m
@@ -14,14 +14,21 @@
 #define maxValue 1.0
 #define startValue (maxValue - minValue)/2
 
+#define sliderH 40
+@interface CustomSlider ()
+@property (strong, nonatomic) UIView* testView;
+@property (strong, nonatomic) UIImageView* imageView;
+@property (assign, nonatomic) CGFloat startPosition;
+@property (assign, nonatomic) CGFloat finishPosition;
+- (void)setTarget:(id)target;
+@end
 @implementation CustomSlider
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-       [self setThumbImage:[imagePNG(@"handleFilter")stretchableImageWithLeftCapWidth:15 topCapHeight:15] forState:UIControlStateNormal];
-        
+       [self setThumbImage:imagePNG(@"handleFilter") forState:UIControlStateNormal];
         UIImage* stretchLeftTrack = imagePNG(@"trackFilter"),*stretchRightTrack = imagePNG(@"trackFilter");
         
         if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"6.0")) {
@@ -36,8 +43,75 @@
         self.maximumValue = maxValue;
         self.value = startValue;
         self.userInteractionEnabled = YES;
+        
+        NSLog(@"init x %f", [self xPositionFromSliderValue:self]);
+        self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake((self.bounds.origin.x + self.bounds.size.width) / 2 - 25 /2, -5, 33, sliderH)];
+        [self.imageView setImage:[imagePNG(@"handleFilter")stretchableImageWithLeftCapWidth:15 topCapHeight:15]];
+        [self addSubview:self.imageView];
+        [self setTarget:self];
+        
+        self.startPosition = [self xPositionFromSliderValue:self];
+        NSLog(@"startPosition :  %f", self.startPosition);
+        self.finishPosition = [self xPositionFromSliderValue:self];
+        
+        [self moving];
     }
     return self;
 }
 
+- (CGRect)thumbRectForBounds:(CGRect)bounds trackRect:(CGRect)rect value:(float)value
+{
+    return [super thumbRectForBounds:bounds trackRect:rect value:value];
+}
+
+- (float)xPositionFromSliderValue:(UISlider *)aSlider;
+{
+    float sliderRange = aSlider.frame.size.width - aSlider.currentThumbImage.size.width;
+    
+    float sliderValueToPixels = (((aSlider.value-aSlider.minimumValue)/(aSlider.maximumValue-aSlider.minimumValue)) * sliderRange);
+    
+    return roundf( sliderValueToPixels);
+}
+
+- (void)moving
+{
+    self.finishPosition = [self xPositionFromSliderValue:self] ;
+    
+    CGFloat distance;
+    if (self.finishPosition > self.startPosition) {
+        distance = abs(self.finishPosition - self.startPosition);
+    } else {
+        distance = abs(self.startPosition - self.finishPosition);
+    }
+    
+    NSLog(@"distance :  %f", distance);
+    NSLog(@"x  %f", self.finishPosition - self.currentThumbImage.size.width / 2);
+    if (self.finishPosition > self.startPosition) {
+        self.imageView.frame = CGRectMake(self.startPosition - 0*self.currentThumbImage.size.width /2,self.imageView.frame.origin.y, distance + self.currentThumbImage.size.width, sliderH);
+    } else {
+        self.imageView.frame = CGRectMake(self.finishPosition - 0*self.currentThumbImage.size.width /2 , self.imageView.frame.origin.y, distance + self.currentThumbImage.size.width, sliderH);
+    }
+}
+
+- (void)startMoving:(id)sender
+{
+    self.startPosition = [self xPositionFromSliderValue:sender];
+    NSLog(@"startPosition :  %f", self.startPosition);
+}
+
+- (void)finishMoving:(id)sender
+{
+    self.finishPosition = [self xPositionFromSliderValue:sender];
+    NSLog(@"finishPosition :  %f", self.finishPosition);
+    self.imageView.frame = CGRectMake( self.finishPosition - 0*self.currentThumbImage.size.width / 2, self.imageView.frame.origin.y, 33, sliderH);
+    self.startPosition = self.finishPosition;
+}
+
+- (void)setTarget:(id)target
+{
+    [self addTarget:target action:@selector(startMoving:) forControlEvents:UIControlEventTouchDown];
+    [self addTarget:target action:@selector(finishMoving:) forControlEvents:UIControlEventTouchUpInside];
+    [self addTarget:target action:@selector(finishMoving:) forControlEvents:UIControlEventTouchUpOutside];
+    [self addTarget:target action:@selector(moving) forControlEvents:UIControlEventValueChanged];
+}
 @end
--- PrettySlider/PrettySlider/ViewController.m
+++ PrettySlider/PrettySlider/ViewController.m
@@ -11,10 +11,6 @@
 
 @interface ViewController ()
 @property (strong, nonatomic) CustomSlider* slider;
-@property (strong, nonatomic) UIImageView* imageView;
-
-@property (assign, nonatomic) CGFloat startPosition;
-@property (assign, nonatomic) CGFloat finishPosition;
 @end
 
 @implementation ViewController
@@ -24,68 +20,6 @@
     [super viewDidLoad];
     self.slider = [[CustomSlider alloc] initWithFrame:CGRectMake(20, 100, 280, 30)];
     [self.view addSubview:self.slider];
-    
-    [self.slider addTarget:self action:@selector(startMoving:) forControlEvents:UIControlEventTouchDown];
-    [self.slider addTarget:self action:@selector(finishMoving:) forControlEvents:UIControlEventTouchUpInside];
-    [self.slider addTarget:self action:@selector(finishMoving:) forControlEvents:UIControlEventTouchUpOutside];
-    [self.slider addTarget:self action:@selector(moving:) forControlEvents:UIControlEventValueChanged];
-    
-    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(145, 95, 33, 40)];
-    [self.imageView setImage:[imagePNG(@"handleFilter")stretchableImageWithLeftCapWidth:15 topCapHeight:15]];
-    [self.view addSubview:self.imageView];
-    
-    self.startPosition = [self xPositionFromSliderValue:self.slider];    
-    NSLog(@"startPosition :  %f", self.startPosition);
-    self.finishPosition = [self xPositionFromSliderValue:self.slider] + self.slider.currentThumbImage.size.width;
-    NSLog(@"finishPosition :  %f", self.finishPosition);
-
 }
 
-- (float)xPositionFromSliderValue:(UISlider *)aSlider;
-{
-    float sliderRange = aSlider.frame.size.width - aSlider.currentThumbImage.size.width;
-    float sliderOrigin = aSlider.frame.origin.x + (aSlider.currentThumbImage.size.width / 2.0);
-    
-    float sliderValueToPixels = (((aSlider.value-aSlider.minimumValue)/(aSlider.maximumValue-aSlider.minimumValue)) * sliderRange) + sliderOrigin;
-
-    return sliderValueToPixels;
-}
-
-- (void)startMoving:(id)sender
-{
-    self.startPosition = [self xPositionFromSliderValue:sender];
-    NSLog(@"startPosition :  %f", self.startPosition);
-}
-
-- (void)finishMoving:(id)sender
-{
-    self.finishPosition = [self xPositionFromSliderValue:sender];
-    NSLog(@"finishPosition :  %f", self.finishPosition);
-    self.imageView.frame = CGRectMake( self.finishPosition - ((UISlider*)sender).currentThumbImage.size.width / 2, self.imageView.frame.origin.y, 33, 40);
-    self.startPosition = self.finishPosition;
-}
-
-- (void)moving:(id)sender
-{
-    self.finishPosition = [self xPositionFromSliderValue:sender] ;//+ ((UISlider*)sender).currentThumbImage.size.width;
-   
-    CGFloat distance;
-    if (self.finishPosition > self.startPosition) {
-        distance = abs(self.finishPosition - self.startPosition);
-    } else {
-        distance = abs(self.startPosition - self.finishPosition);
-    }
-
-//    if (abs(distance) < 33) {
-//        distance = 33;
-//    }
-    
-    NSLog(@"distance :  %f", distance);
-    NSLog(@"x  %f", self.finishPosition - ((UISlider*)sender).currentThumbImage.size.width / 2);
-    if (self.finishPosition > self.startPosition) {
-        self.imageView.frame = CGRectMake(self.startPosition - ((UISlider*)sender).currentThumbImage.size.width / 2 , self.imageView.frame.origin.y, distance +((UISlider*)sender).currentThumbImage.size.width, 40);
-    } else {
-        self.imageView.frame = CGRectMake(self.finishPosition - ((UISlider*)sender).currentThumbImage.size.width /2 , self.imageView.frame.origin.y, distance +((UISlider*)sender).currentThumbImage.size.width, 40);
-    }
-}
 @end
