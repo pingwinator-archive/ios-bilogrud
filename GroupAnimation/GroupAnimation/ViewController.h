c8e0b3766b4288493465ab21abd9d394b759370b
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2013-03-15 15:38:29 +0200

    init group animation

---
+++ GroupAnimation/GroupAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GroupAnimation
+//
+//  Created by Natasha on 15.03.13.
+//  Copyright (c) 2013 Natasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
