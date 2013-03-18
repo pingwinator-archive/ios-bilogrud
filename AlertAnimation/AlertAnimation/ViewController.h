76605a2acfb3ae43ede7b3951b9d4338699d3378
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2013-03-18 23:35:39 +0200

    init alert animation project

---
+++ AlertAnimation/AlertAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlertAnimation
+//
+//  Created by Natasha on 18.03.13.
+//  Copyright (c) 2013 Natasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
