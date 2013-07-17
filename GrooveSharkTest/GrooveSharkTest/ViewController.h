aa3a3ffe85bb1407cc4e4adc596093aad21b450d
Natalia Bilogrud <nata.bilogrud@postindustria.com>
2013-07-17 15:49:29 +0300

    init commit - add GrooveShark test project

---
+++ GrooveSharkTest/GrooveSharkTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GrooveSharkTest
+//
+//  Created by Natasha on 17.07.13.
+//  Copyright (c) 2013 Natasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
